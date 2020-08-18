acbf9cbc8243a1ac0f1b1b6de81cbd6990b33559
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-18 02:03:56 +0000

    Automatically backed up by Learn

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,3 +1,5 @@
-def plus_two(num)
-	num + 2
-end
+def plus_two(num)   #calling the plus_two method with argument num
+	num + 2    
+#	num      #here the method is returning the original number, when it needs to return the sum of the orignal number plus two
+#	binding.pry    #when we use binding.pry here we see that num is set to 3, therefore the method is returning 3
+end 
